9ad53a1ba80b9cd757bba47b8eec1a4356f65558
Renan Luis Bianchini <renan.bianchini@southsystem.com.br>
2022-08-03 14:25:29 -0300

    Primeiro Commit

---
+++ 02-operacoes-matematicas/OperacoesMatematicas.java
@@ -0,0 +1,47 @@
+import java.util.logging.Logger;
+
+public class OperacoesMatematicas {
+    Logger logger = Logger.getLogger("Teste");
+
+
+    // Somar dois números
+    public static int somarDoisNumeros(int x, int y) {
+        return 0;
+    }
+
+    // Subtrair dois números
+    public static int subtrairTresNumeros(int x, int y, int z) {
+        return 0;
+    }
+
+    // Multiplicar dois números
+    public static int multiplicarDoisNumeros(int x, int y) {
+        return 0;
+    }
+
+    // Dividir dois números
+    public static double dividirDoisNumeros(int x, int y) {
+        return 0;
+    }
+
+    // Elevar o primeiro número ao segundo
+    public static int elevarUmNumeroAOutro(int x, int y) {
+        return 0;
+    }
+
+    // Mostrar o resto da divisão de dois números
+    public static int restoDivisaoDoisNumeros(int x, int y) {
+        return 0;
+    }
+
+    // Método main que executa as ações da classe
+    public static void main(String[] args) {
+        somarDoisNumeros(0, 0);
+        subtrairTresNumeros(0, 0, 0);
+        multiplicarDoisNumeros(0, 0);
+        dividirDoisNumeros(0, 0);
+        elevarUmNumeroAOutro(0, 0);
+        restoDivisaoDoisNumeros(0, 0);
+    }
+    
+}
